f4e95778085c4e3e2f26830a818582d773631ae6
zobayertihan <zobayertihan@gmail.com>
2022-10-12 01:53:40 +0600

    Toast

---
+++ src/components/Question/Question.js
@@ -0,0 +1,32 @@
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Question = ({ qus, val }) => {
+    const { question, options, correctAnswer } = qus
+    const notify = (ans) => {
+        if (JSON.stringify(ans) === JSON.stringify(correctAnswer)) {
+            toast("Right Ans")
+        }
+        else {
+            toast("Wrong Ans")
+        }
+
+    };
+
+
+    return (
+        <div>
+            <h2>Quiz  { }: {question}</h2>
+            <div>
+                <button >{correctAnswer}</button>
+
+            </div>
+            {
+                options.map((option, index) => <button onClick={(e) => notify(e.target.innerText)} key={index}>{option}</button>)
+
+            }
+        </div >
+    );
+};
+
+export default Question;
