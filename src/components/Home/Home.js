f0d11572a5f7dd2fb170c568ac30ee9a406a9fcb
zobayertihan <zobayertihan@gmail.com>
2022-10-12 01:51:09 +0600

    HomePage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,23 @@
-// import React, { useState } from 'react';
-// import { useLoaderData } from 'react-router-dom';
-// import Topic from '../Topic/Tpoic';
-// import './Home.css'
+import React, { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Topic from '../Topic/Tpoic';
+import './Home.css'
 
-// const Home = () => {
-//     const topics = useLoaderData().data;
-//     return (
-//         <div>
-//             <h2>Welcome Home</h2>
-//             <div className='home-container'>
-//                 {
-//                     topics.map(topic => <Topic
-//                         key={topic.id}
-//                         topic={topic}
-//                     ></Topic>)
-//                 }
-//             </div>
-//         </div>
-//     );
-// };
+const Home = () => {
+    const topics = useLoaderData().data;
+    return (
+        <div>
+            <h2>Welcome Home</h2>
+            <div className='home-container'>
+                {
+                    topics.map(topic => <Topic
+                        key={topic.id}
+                        topic={topic}
+                    ></Topic>)
+                }
+            </div>
+        </div>
+    );
+};
 
-// export default Home;
+export default Home;
