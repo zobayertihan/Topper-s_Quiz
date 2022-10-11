77794a9f288a3b1d574d58a9514e37bbaffae4c1
zobayertihan <zobayertihan@gmail.com>
2022-10-12 01:53:11 +0600

    Added Questions

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,23 +1,23 @@
-// import React from 'react';
-// import { useLoaderData } from 'react-router-dom';
-// import Question from '../Question/Question';
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Question from '../Question/Question';
 
 
-// const Questions = () => {
-//     const questions = useLoaderData().data.questions;
-//     console.log(questions)
-//     const val = questions.length
-//     return (
-//         <div>
-//             {
-//                 questions.map(qus => <Question
-//                     key={qus.id}
-//                     qus={qus}
-//                     val={val}
-//                 ></Question>)
-//             }
-//         </div>
-//     );
-// };
+const Questions = () => {
+    const questions = useLoaderData().data.questions;
+    console.log(questions)
+    const val = questions.length
+    return (
+        <div>
+            {
+                questions.map(qus => <Question
+                    key={qus.id}
+                    qus={qus}
+                    val={val}
+                ></Question>)
+            }
+        </div>
+    );
+};
 
-// export default Questions;
+export default Questions;
